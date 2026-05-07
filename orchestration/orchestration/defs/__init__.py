a0bd2b1d5fb49cffed88f2e10fcf66bf06e8cc7e
Daniel Conner <daniel.w.conner@gmail.com>
2026-05-06 19:17:18 -0700

    Debugging defs

--- orchestration/orchestration/defs/__init__.py
+++ orchestration/orchestration/defs/__init__.py
@@ -1,10 +1,1 @@
-from dagster import Definitions, load_assets_from_modules
 
-from . import ingest
-from . import process
-from . import features
-all_assets = load_assets_from_modules([ingest,process,features])
-
-defs = Definitions(
-    assets=all_assets,
-)
