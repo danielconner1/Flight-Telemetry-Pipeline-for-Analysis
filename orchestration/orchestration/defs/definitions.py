a5817232dbfb6530279d09d610c4379db30f265e
Daniel Conner <daniel.w.conner@gmail.com>
2026-05-13 15:45:53 -0700

    Fix Dagster job and schedule registration

---
+++ orchestration/orchestration/defs/definitions.py
@@ -0,0 +1,10 @@
+from dagster import define_asset_job, ScheduleDefinition, Definitions
+from .jobs import telemetry_job, telemetry_schedule
+from .ingest import ingest_raw_csv_to_parquet
+from .process import process
+from .features import features
+
+defs = Definitions(assets=[ingest_raw_csv_to_parquet, process, features],
+                   schedules=[telemetry_schedule],
+                   jobs=[telemetry_job]
+                   )
